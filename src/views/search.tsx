c3fca92bb19b5a247dad1682c961b9a29e1efc8b
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2020-06-02 20:43:38 +0200

    add: Search bar within search view

--- src/views/search.tsx
+++ src/views/search.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import {Text, TextInput, View} from 'react-native'
 
 import { SafeAreaView } from 'react-native-safe-area-context'
+
+import Constants from '../constants'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface SearchState {}
@@ -15,9 +17,33 @@ class SearchView extends React.Component<SearchProps, SearchState> {
 
   render() {
     return (
-      <SafeAreaView>
-        <View>
-          <Text>{'SearchView'}</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            width: Constants.screenWidth,
+            height: 100,
+            flex: 0.4,
+            borderBottomWidth: 0.5,
+            borderBottomColor: 'grey',
+            alignItems: 'center'
+          }}>
+          <TextInput
+            style={{
+              height: 40,
+              width: Constants.screenWidth * 0.9,
+              borderColor: 'gray',
+              borderWidth: 1,
+              borderRadius: 20
+            }}
+          />
+        </View>
+        <View
+          style={{
+            width: Constants.screenWidth,
+            height: 200,
+            flex: 5.6
+          }}>
+          <Text>{''}</Text>
         </View>
       </SafeAreaView>
     )
