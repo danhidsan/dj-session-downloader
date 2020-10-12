72232d85830112badd17b813439b802b5008354a
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2020-10-12 15:26:06 +0200

    refactor: create navigation root stack

--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,43 @@
 import React from 'react'
 import { Text, View, SafeAreaView } from 'react-native'
-
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { ActionSheetProvider } from '@expo/react-native-action-sheet'
+import { enableScreens } from 'react-native-screens'
 
 // Views
 import MusicView from './views/Music'
 import SearchView from './views/Search'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface MainState {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface MainProps {}
+enableScreens()
 
+const RootStack = createStackNavigator()
 const Tab = createBottomTabNavigator()
 
-class MainView extends React.Component<MainProps, MainState> {
-  constructor(props: MainProps) {
-    super(props)
-  }
+const Home = () => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Music"
+      component={MusicView}
+      options={{ tabBarLabel: 'Music' }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={SearchView}
+      options={{ tabBarLabel: 'Search' }}
+    />
+  </Tab.Navigator>
+)
 
-  render() {
-    return (
-      <ActionSheetProvider>
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen
-              name="Music"
-              component={MusicView}
-              options={{ tabBarLabel: 'Music' }}
-            />
-            <Tab.Screen
-              name="Search"
-              component={SearchView}
-              options={{ tabBarLabel: 'Search' }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </ActionSheetProvider>
-    )
-  }
-}
+const Main = () => (
+  <ActionSheetProvider>
+    <NavigationContainer>
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        <RootStack.Screen name="Home" component={Home}/>
+      </RootStack.Navigator>
+    </NavigationContainer>
+  </ActionSheetProvider>
+)
 
-export default MainView
+export default Main
